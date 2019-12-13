4b967de914f84f241d08ba28b046c386afc0fe7e
Shota Ochi <shotaochi1990@gmail.com>
2019-12-14 06:21:54 +0900

    add a video to test

---
+++ R/extdata.R
@@ -0,0 +1,9 @@
+#' Video of "Bee on a flower"
+#'
+#' This video was downloaded from \url{https://mixkit.co/free-stock-video/bee-flower-1000/}.
+#' We can use this video under Mixkit license. See \url{https://mixkit.co/license/} for detail.
+#' @source \url{https://mixkit.co/free-stock-video/bee-flower-1000/}
+#' @name bees.mp4
+#' @examples
+#' system.file("extdata", "bees.mp4", package = "videor")
+NULL
