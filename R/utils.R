d316d2a655cf051854f7a4077786349d18983642
Shota Ochi <shotaochi1990@gmail.com>
2019-12-17 07:31:08 +0900

    add new arguments into load_video

--- R/utils.R
+++ R/utils.R
@@ -1,7 +1,7 @@
 assert_video <- function(video)
 {
   assert_access(video, access = 'r', .var.name = deparse(substitute(video)))
-  assert_character(video, len = 1, .var.name = deparse(substitute(video)))
+  assert_character(video, any.missing = FALSE, len = 1, .var.name = deparse(substitute(video)))
 }
 
 assert_nb_frames <- function(nb_frames)
@@ -13,4 +13,14 @@ assert_nb_frames <- function(nb_frames)
 assert_frames <- function(frames, nb_frames)
 {
   assert_numeric(frames, lower = 1, upper = nb_frames, finite = TRUE, any.missing = FALSE, min.len = 1, max.len = 2, sorted = TRUE, .var.name = deparse(substitute(frames)))
+}
+
+assert_step <- function(step)
+{
+  assert_numeric(step, lower = 1, finite = TRUE, any.missing = FALSE, len = 1, .var.name = deparse(substitute(step)))
+}
+
+assert_maxSize <- function(maxSize)
+{
+  assert_numeric(maxSize, lower = 0, finite = TRUE, any.missing = FALSE, len = 1, .var.name = deparse(substitute(maxSize)))
 }
--- tests/testthat/test_load_video.R
+++ tests/testthat/test_load_video.R
@@ -1,5 +1,9 @@
 test_that("load_video",
 {
+  expect_error(load_video(path_wrong, 1))
+  expect_error(load_video(path_wrong2, 1))
+  
+  # bees.mp4
   bees_1 <- load_video(bees, 1)
   bees_2 <- load_video(bees, 2)
   bees_3 <- load_video(bees, 3)
@@ -7,14 +11,28 @@ test_that("load_video",
   bees_5 <- load_video(bees, 5)
   bees_6 <- load_video(bees, 6)
   bees_1to6 <- load_video(bees, c(1,6))
-  
-  expect_error(load_video(path_wrong, 1))
-  expect_error(load_video(path_wrong2, 1))
-  
+  bees_2to6_step2 <- load_video(bees, c(1,6), step = 2)
+  expect_error(load_video(bees, 1, step = NA))
+  expect_error(load_video(bees, 1, expr = 100))
+  expect_error(load_video(bees, 1, maxSize = "A"))
   expect_class(bees_1, class_imager)
   expect_class(bees_1to6, class_imager)
-
   expect_equal_cimg(bees_1, frames(bees_1to6, 1)[[1]])
   expect_equal_cimg(bees_4, frames(bees_1to6, 4)[[1]])
-  expect_equal_cimg(bees_6, frames(bees_1to6, 6)[[1]]) 
+  expect_equal_cimg(bees_6, frames(bees_1to6, 6)[[1]])
+  expect_equal_cimg(bees_2, frames(bees_2to6_step2, 2)[[1]])
+  expect_equal_cimg(bees_4, frames(bees_2to6_step2, 4)[[1]])
+  expect_equal_cimg(bees_6, frames(bees_2to6_step2, 6)[[1]])
+  expect(all(!is.na(bees_1)))
+  expect_equal(depth(bees_1to6), 6)
+  expect_equal(depth(bees_2to6_step2), 3)
+  
+  # counts.gif
+  counts_1 <- load_video(counts, 1)
+  counts_4 <- load_video(counts, 4)
+  counts_9 <- load_video(counts, 9)
+  counts_all <- load_video(counts)
+  expect_equal_cimg(counts_1, frames(counts_all, 1)[[1]])
+  expect_equal_cimg(counts_4, frames(counts_all, 4)[[1]])
+  expect_equal_cimg(counts_9, frames(counts_all, 9)[[1]])
 })
