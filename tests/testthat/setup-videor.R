d99da6e0c8b06a3296ee44ffd9ac07742f82c4ce
Shota Ochi <shotaochi1990@gmail.com>
2019-12-16 22:45:53 +0900

    add necessary file

--- tests/testthat/setup-videor.R
+++ tests/testthat/setup-videor.R
@@ -5,6 +5,7 @@ class_imager <- "cimg"
 class_pixset <- "pixset"
 
 bees <- system.file("extdata", "bees.mp4", package = "videor")
+counts <- system.file("extdata", "counts.gif", package = "videor")
 path_wrong <- "wrong path"
 path_wrong2 <- c(bees, bees)
 
